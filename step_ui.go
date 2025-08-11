533f4d06b615161aa0e885993f84b947a09f6bb9
余泓铮 <yuhongzheng@bytedance.com>
2025-08-11 21:24:16 +0800

    feat: 支持获取剪贴版

--- step_ui.go
+++ step_ui.go
@@ -1043,22 +1043,6 @@ func runStepMobileUI(s *SessionRunner, step IStep) (stepResult *StepResult, err 
 				continue
 			}
 
-			if action.Method == option.ACTION_StartToGoal {
-				planningResults, err := uiDriver.StartToGoal(ctx,
-					action.Params.(string), action.GetOptions()...)
-				actionResult.Elapsed = time.Since(actionStartTime).Milliseconds()
-				actionResult.Plannings = planningResults
-				stepResult.Actions = append(stepResult.Actions, actionResult)
-				if err != nil {
-					actionResult.Error = err.Error()
-					if !code.IsErrorPredefined(err) {
-						err = errors.Wrap(code.MobileUIDriverError, err.Error())
-					}
-					return stepResult, err
-				}
-				continue
-			}
-
 			// handle AI operations (ai_action, ai_query, ai_assert) with unified result storage
 			if action.Method == option.ACTION_AIAction || action.Method == option.ACTION_Query || action.Method == option.ACTION_AIAssert {
 				var aiResult *uixt.AIExecutionResult
